643a4a8decb720e90e9ff64bccd8649e439fa45f
MacBookUz <macbookuz@MacBook-Pro-MacBookUz.local>
2025-05-11 13:29:03 +0500

    home page is finished

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,13 +1,21 @@
 "use client";
-import { SignOutButton } from "@clerk/nextjs";
-import React, { use, useEffect, useState } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
-
+import { ArrowRight, LogIn } from "lucide-react";
+import Link from "next/link";
+import { Tooltip } from "react-tooltip";
+import { FiHome, FiUser, FiShoppingCart, FiInfo } from "react-icons/fi";
+import "react-tooltip/dist/react-tooltip.css";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
   const [phone, setPhone] = useState("");
+  const [activeLink, setActiveLink] = useState<string>("home");
+
   useEffect(() => {
     getPhone();
   }, []);
+
   async function getPhone() {
     const { data } = await supabase.from("settings").select("phone").single();
     const phone = data?.phone;
@@ -15,32 +23,165 @@ const Navbar = () => {
   }
 
   return (
-    <div className="flex justify-between items-center px-10">
-      <div className="flex items-center">
-        <img className="w-25" src="barber.png" alt="logo" />
-        <h1 className="text-3xl">Trimly</h1>
+    <>
+      <div className="hidden lg:flex justify-between items-center px-10 text-white relative z-100 border-b border-white pb-4">
+        <div className="flex items-center mt-3">
+          <img className="w-25" src="barberWhite.png" alt="logo" />
+          <h1 className="text-3xl">Trimly</h1>
+        </div>
+        <div className="pt-3 flex items-center gap-7">
+          <ul className="flex items-center gap-7">
+            <li>
+              <Link
+                href="/"
+                className={`hover:text-[#c8865c] transition-colors ${
+                  activeLink === "home" ? "text-[#c8865c]" : ""
+                }`}
+                onClick={() => setActiveLink("home")}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/masters"
+                className={`hover:text-[#c8865c] transition-colors ${
+                  activeLink === "masters" ? "text-[#c8865c]" : ""
+                }`}
+                onClick={() => setActiveLink("masters")}
+              >
+                Masters
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/services"
+                className={`hover:text-[#c8865c] transition-colors ${
+                  activeLink === "services" ? "text-[#c8865c]" : ""
+                }`}
+                onClick={() => setActiveLink("services")}
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about-us"
+                className={`hover:text-[#c8865c] transition-colors ${
+                  activeLink === "about-us" ? "text-[#c8865c]" : ""
+                }`}
+                onClick={() => setActiveLink("about-us")}
+              >
+                About us
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="pt-1.5 flex items-center gap-4">
+          <span>Phone : {phone}</span>
+          <div className="flex items-center gap-5">
+            <ClerkProvider>
+              <SignedOut>
+                <Link href="/sign-in">
+                  <div
+                    data-tooltip-id="login-tooltip"
+                    data-tooltip-content="Login"
+                    className="cursor-pointer"
+                  >
+                    <LogIn size={24} />
+                  </div>
+                </Link>
+                <Tooltip
+                  id="login-tooltip"
+                  place="bottom"
+                  className="text-xs px-2 py-1 text-black border border-gray-300 shadow"
+                  style={{
+                    fontSize: "12px",
+                    backgroundColor: "#fff",
+                    color: "#000",
+                  }}
+                />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </ClerkProvider>
+            <Link
+              href={"/profile"}
+              className="p-3 px-4 bg-[#c8865c] text-white flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:shadow-lg hover:bg-[#b4744e]"
+            >
+              Book a visit <ArrowRight />
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="pt-1.5">
-        <ul className="flex items-center gap-7">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/masters">Masters</a>
-          </li>
-          <li>
-            <a href="/masters">Services</a>
-          </li>
-          <li>
-            <a href="/masters">About Us</a>
-          </li>
-        </ul>
+
+      {/* Mobil navbar */}
+      <div className="lg:hidden fixed bottom-0 w-full h-20 bg-[#c8865c] text-white flex justify-around items-center py-2 z-50">
+        <Link
+          href="/"
+          className={`transition-transform ${
+            activeLink === "home" ? "scale-125 text-white" : "text-white/70"
+          }`}
+          onClick={() => setActiveLink("home")}
+        >
+          <FiHome size={24} />
+        </Link>
+        <Link
+          href="/masters"
+          className={`transition-transform ${
+            activeLink === "masters" ? "scale-125 text-white" : "text-white/70"
+          }`}
+          onClick={() => setActiveLink("masters")}
+        >
+          <FiUser size={24} />
+        </Link>
+        <Link
+          href="/services"
+          className={`transition-transform ${
+            activeLink === "services" ? "scale-125 text-white" : "text-white/70"
+          }`}
+          onClick={() => setActiveLink("services")}
+        >
+          <FiShoppingCart size={24} />
+        </Link>
+        <Link
+          href="/about-us"
+          className={`transition-transform ${
+            activeLink === "about-us" ? "scale-125 text-white" : "text-white/70"
+          }`}
+          onClick={() => setActiveLink("about-us")}
+        >
+          <FiInfo size={24} />
+        </Link>
+        <ClerkProvider>
+          <SignedOut>
+            <Link href="/sign-in">
+              <div
+                data-tooltip-id="login-tooltip"
+                data-tooltip-content="Login"
+                className="cursor-pointer"
+              >
+                <LogIn size={24} />
+              </div>
+            </Link>
+            <Tooltip
+              id="login-tooltip"
+              place="bottom"
+              className="text-xs px-2 py-1 text-black border border-gray-300 shadow"
+              style={{
+                fontSize: "12px",
+                backgroundColor: "#fff",
+                color: "#000",
+              }}
+            />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </ClerkProvider>
       </div>
-      <div className="pt-1.5 flex items-center gap-4">
-        <span>Phone : {phone}</span>
-        <SignOutButton />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Navbar from "./navbar/page";
+import { ArrowRight } from "lucide-react";
+import { redirect } from "next/navigation";
+import Section1 from "./section1/page";
+import Section2 from "./section2/page";
+import Section3 from "./section3/page";
+import Footer from "./footer/page";
 
-const images = ["/bgbarber1.png", "/bgbarber2.png", "/bgbarber3.png"];
+const images = ["/bgbarber5.png", "/barberbg4.png", "/bgbarber3.png"];
 
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,16 +22,16 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <Navbar />
-
+    <div className="overflow-hidden">
       <div
-        className="w-full h-[100vh] bg-cover bg-center relative transition-all duration-1000"
+        className="w-full h-screen bg-cover bg-center relative transition-all duration-1000"
         style={{
           backgroundImage: `url(${images[currentIndex]})`,
         }}
       >
-        {/* 🔲 Overlay */}
+        <Navbar />
+
+        {/* Overlay */}
         <div
           className="absolute inset-0 z-10"
           style={{
@@ -33,15 +39,54 @@ const Home = () => {
           }}
         />
 
-        <div className="relative z-20 flex items-center justify-center h-full">
-          <div>
-            <h1 className="text-white text-4xl font-bold drop-shadow-lg">
+        <div className="relative z-20 flex items-start justify-between px-10 py-20 flex-wrap h-full text-center md:text-start">
+          <div className="max-w-[600px] flex flex-col gap-5 items-center sm:items-start">
+            <h1 className="text-white text-5xl md:text-7xl sm:text-6xl font-bold drop-shadow-lg">
               Specializing in the latest modern short hair styles
             </h1>
-            <p>It's all about the pleasure to feel yourself.</p>
+            <div>
+              <span className="text-white font-bold text-center md:text-start ">
+                It's all about the pleasure to feel yourself.
+              </span>
+            </div>
+            <div>
+              <button
+                onClick={() => redirect("/profile")}
+                className="p-3 px-5 bg-[#c8865c] flex text-white gap-2 transition-all duration-300 hover:scale-105 hover:shadow-lg hover:bg-[#b4744e]"
+              >
+                Learn More <ArrowRight />
+              </button>
+            </div>
+          </div>
+
+          <div className="flex gap-20 mt-3 pl-4">
+            <div className="flex flex-col gap-4 md:gap-6">
+              <h1 className="text-white text-xl md:text-2xl font-semibold">
+                Locations
+              </h1>
+              <div className="text-white text-[13px] md:text-sm">
+                <p>Mustaqillik</p>
+                <p>Alpomish</p>
+                <p>Piridastgir</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 md:gap-6">
+              <h1 className="text-white text-xl md:text-2xl font-semibold">
+                Hours
+              </h1>
+              <div className="text-white text-[11px] md:text-sm">
+                <p>Mon-Fri: 9AM - 8PM</p>
+                <p>Sat: 9AM - 6PM</p>
+                <p>Sun: 10AM - 5PM</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Footer />
     </div>
   );
 };
